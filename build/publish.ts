23f471d3acf8d9df069806b09fc20e9d85d5bef5
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-05-03 21:13:45 +0800

    chore: updated publish script

--- build/publish.ts
+++ build/publish.ts
@@ -4,7 +4,7 @@ import { runTask } from './utils'
 export default series(
   runTask('publish @baiwusanyu/utils-log', 'cd dist/log && pnpm run publish:npm'),
   runTask('publish @baiwusanyu/utils-task', 'cd dist/task && pnpm run publish:npm'),
-  runTask('publish @baiwusanyu/utils-obj', 'cd dist/task && pnpm run publish:npm'),
-  runTask('publish @baiwusanyu/utils-is', 'cd dist/task && pnpm run publish:npm'),
+  runTask('publish @baiwusanyu/utils-obj', 'cd dist/obj && pnpm run publish:npm'),
+  runTask('publish @baiwusanyu/utils-is', 'cd dist/is && pnpm run publish:npm'),
   runTask('publish baiwusanyu-utils', 'cd dist && pnpm run publish:npm'),
 )
