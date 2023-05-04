c176f38f8e3a4fa3066f86aa7fbfa48cd4c3a5fe
baiwusanyu-c <740132583@qq.com>
2023-05-04 16:09:04 +0800

    feat: added normalizeEllipsis function

--- packages/normalize/src/__test__/normalize.spec.ts
+++ packages/normalize/src/__test__/normalize.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import { normalizePath } from '../normalize'
+import { normalizeEllipsis, normalizePath } from '../normalize'
 describe('normalize', () => {
   test('normalizePath replaces backslashes with forward slashes', () => {
     const path = 'C:\\Users\\John\\Documents\\file.txt'
@@ -20,5 +20,40 @@ describe('normalize', () => {
     const expected = 'C:/Users/John/Documents/file.txt'
     const result = normalizePath(path)
     expect(result).toBe(expected)
+  })
+
+  test('returns empty string when input is empty', () => {
+    const result = normalizeEllipsis('')
+    expect(result).toEqual('')
+  })
+
+  test('returns input string when input is already shorter than limit', () => {
+    const result = normalizeEllipsis('hello world', 20)
+    expect(result).toEqual('hello world')
+  })
+
+  test('adds ellipsis when input is longer than limit', () => {
+    const result = normalizeEllipsis('hello world', 5)
+    expect(result).toEqual('hello...')
+  })
+
+  test('adds ellipsis when input contains non-ascii characters', () => {
+    const result = normalizeEllipsis('你好，世界！', 5)
+    expect(result).toEqual('你好...')
+  })
+
+  test('adds ellipsis when input contains mixed ascii and non-ascii characters', () => {
+    const result = normalizeEllipsis('hello 世界！', 6)
+    expect(result).toEqual('hello ...')
+  })
+
+  test('returns input string without ellipsis when input contains exactly limitLen characters', () => {
+    const result = normalizeEllipsis('hello world', 11)
+    expect(result).toEqual('hello world')
+  })
+
+  test('returns input string without ellipsis when input contains exactly (limitLen-1) characters', () => {
+    const result = normalizeEllipsis('hello world', 10)
+    expect(result).toEqual('hello worl...')
   })
 })
--- packages/normalize/src/normalize.ts
+++ packages/normalize/src/normalize.ts
@@ -1,3 +1,18 @@
 export function normalizePath(path: string): string {
   return path.replace(/[\\/]/g, '/')
 }
+
+export function normalizeEllipsis(str = '', limitLen = 24) {
+  let len = 0
+  // eslint-disable-next-line no-control-regex
+  const reg = /[\x00-\xFF]/ // 半角字符的正则匹配
+  const strContent = str.split('')
+  const inx = strContent.findIndex((s) => {
+    len += reg.test(s) ? 1 : 2
+    if (len > limitLen)
+      return true
+    else
+      return false
+  })
+  return inx === -1 ? str : `${str.substr(0, inx)}...`
+}
