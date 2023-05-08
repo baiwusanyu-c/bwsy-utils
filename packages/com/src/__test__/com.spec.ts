f891c7c91faa5e310c549ba503498d364bab2031
baiwusanyu-c <740132583@qq.com>
2023-05-08 10:23:35 +0800

    feat: added throttle function and debounce function

--- packages/com/src/__test__/com.spec.ts
+++ packages/com/src/__test__/com.spec.ts
@@ -1,5 +1,17 @@
-import { describe, expect, test, vi } from 'vitest'
-import { createHash, downloadFile } from '../com'
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  test,
+  vi,
+} from 'vitest'
+import {
+  createHash,
+  debounce,
+  downloadFile,
+  throttle,
+} from '../com'
 
 describe('downloadFile', () => {
   test('should create an <a> element with the given URL and click it', () => {
@@ -31,5 +43,80 @@ describe('createHash', () => {
     expect(createHash()).toBeTruthy()
     expect(createHash('test')).toBeTruthy()
     expect(createHash(123456)).toBeTruthy()
+  })
+})
+
+describe('debounce', () => {
+  let mockFn: any
+
+  beforeEach(() => {
+    mockFn = vi.fn()
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.clearAllMocks() // 清空所有的 mock 函数调用信息
+    vi.useRealTimers() // 恢复原本的 timer
+  })
+
+  test('debounce function calls', () => {
+    const debouncedFn = debounce(mockFn, 1000)
+
+    debouncedFn()
+    expect(mockFn).not.toHaveBeenCalled()
+
+    vi.advanceTimersByTime(500)
+
+    debouncedFn()
+    expect(mockFn).not.toHaveBeenCalled()
+
+    vi.advanceTimersByTime(1000)
+    expect(mockFn).toHaveBeenCalledTimes(1)
+  })
+
+  test('passes arguments to debounced function', () => {
+    const debouncedFn = debounce(mockFn, 1000)
+
+    debouncedFn('hello', 123)
+    expect(mockFn).not.toHaveBeenCalled()
+
+    vi.advanceTimersByTime(1000)
+
+    expect(mockFn).toHaveBeenCalledWith('hello', 123)
+  })
+})
+
+describe('throttle', () => {
+  let mockFn: any
+
+  beforeEach(() => {
+    vi.useFakeTimers()
+    mockFn = vi.fn()
+  })
+
+  afterEach(() => {
+    vi.clearAllTimers()
+    vi.clearAllMocks()
+    vi.restoreAllMocks()
+  })
+
+  test('should throttle function calls', () => {
+    const throttledFn = throttle(mockFn, 1000)
+
+    throttledFn()
+    expect(mockFn).toHaveBeenCalledTimes(1)
+
+    throttledFn()
+    expect(mockFn).toHaveBeenCalledTimes(1)
+
+    vi.advanceTimersByTime(500)
+
+    throttledFn()
+    expect(mockFn).toHaveBeenCalledTimes(1)
+
+    vi.advanceTimersByTime(600)
+
+    throttledFn()
+    expect(mockFn).toHaveBeenCalledTimes(2)
   })
 })
--- packages/com/src/com.ts
+++ packages/com/src/com.ts
@@ -9,8 +9,50 @@ export const downloadFile = (url: string) => {
   aLink.click()
 }
 
-// TODO 防抖
-// TODO 节流
+/**
+ * 防抖函数
+ * @param func
+ * @param wait
+ */
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: number | undefined
+
+  return function debounced(...args: Parameters<T>): void {
+    if (timeoutId !== undefined)
+      clearTimeout(timeoutId)
+
+    timeoutId = window.setTimeout(() => {
+      func(...args)
+      timeoutId = undefined
+    }, wait)
+  }
+}
+
+/**
+ * 节流函数
+ * @param func
+ * @param wait
+ */
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let isThrottled = false
+
+  return function throttled(...args: Parameters<T>): void {
+    if (!isThrottled) {
+      func(...args)
+      isThrottled = true
+
+      setTimeout(() => {
+        isThrottled = false
+      }, wait)
+    }
+  }
+}
 
 /**
  * 基于 hash sum 生成一个随机哈希
