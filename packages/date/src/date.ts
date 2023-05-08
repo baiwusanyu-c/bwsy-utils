ab041bb44720850c1c536c04d5c45744e9619be6
baiwusanyu-c <740132583@qq.com>
2023-05-08 14:28:33 +0800

    feat: added relativeTime function

--- packages/date/src/date.ts
+++ packages/date/src/date.ts
@@ -1,4 +1,5 @@
 import moment from 'moment'
+import type { unitOfTime } from 'moment'
 /**
  * 补0
  * @param num
@@ -37,4 +38,115 @@ export function formatDate(
     dateObj.utcOffset('+08:00')
 
   return dateObj.format(format)// 格式化日期
+}
+
+/**
+ * 返回一个相对时间 （x小时前/后）
+ * @param cTime
+ * @param rTime
+ * @param isBefore
+ * @param unit
+ * @param lang
+ */
+export function relativeTime(
+  rTime: string,
+  cTime: string,
+  isBefore = true,
+  unit: unitOfTime.StartOf = 'hour',
+  lang = 'zh-cn',
+) {
+  const currentTime = moment(cTime)
+  const relativeTime = moment(rTime)
+  const diff = moment.duration(
+    isBefore
+      ? currentTime.diff(relativeTime)
+      : relativeTime.diff(currentTime),
+  )
+  let res = ''
+  const handleDiff = (unit: unitOfTime.StartOf) => {
+    let finalRes = ''
+    let diffRes = 0
+    switch (unit) {
+      case 'hour':
+        diffRes = Number(diff.asHours().toFixed(0))
+        if (diffRes === 0) {
+          unit = 'minute'
+          finalRes = handleDiff(unit)
+          break
+        }
+        if (Math.abs(diffRes) >= 24) {
+          unit = 'day'
+          finalRes = handleDiff(unit)
+          break
+        }
+        finalRes = `${Math.abs(diffRes)} ${lang === 'zh-cn' ? '小时' : 'hours'}`
+        break
+      case 'day':
+        diffRes = Math.abs(Number(diff.asDays().toFixed(0)))
+        if (diffRes === 0) {
+          unit = 'hour'
+          finalRes = handleDiff(unit)
+          break
+        }
+
+        if (Math.abs(diffRes) >= 7) {
+          unit = 'week'
+          finalRes = handleDiff(unit)
+          break
+        }
+        finalRes = `${Math.abs(diffRes)} ${lang === 'zh-cn' ? '天' : 'days'}`
+        break
+      case 'week':
+        diffRes = Math.abs(Number(diff.asWeeks().toFixed(0)))
+        if (diffRes === 0) {
+          unit = 'day'
+          finalRes = handleDiff(unit)
+          break
+        }
+        if (Math.abs(diffRes) >= 28) {
+          unit = 'month'
+          finalRes = handleDiff(unit)
+          break
+        }
+        finalRes = `${Math.abs(diffRes)} ${lang === 'zh-cn' ? '周' : 'weeks'}`
+        break
+      case 'minute':
+        diffRes = Math.abs(Number(diff.asMinutes().toFixed(0)))
+        if (diffRes === 0) {
+          unit = 'second'
+          finalRes = handleDiff(unit)
+          break
+        }
+        if (Math.abs(diffRes) >= 60) {
+          unit = 'hour'
+          finalRes = handleDiff(unit)
+          break
+        }
+        finalRes = `${Math.abs(diffRes)} ${lang === 'zh-cn' ? '分钟' : 'minutes'}`
+        break
+      case 'second':
+        diffRes = Math.abs(Number(diff.asSeconds().toFixed(0)))
+        finalRes = `${Math.abs(diffRes)} ${lang === 'zh-cn' ? '秒' : 'seconds'}`
+        break
+      case 'month':
+        diffRes = Math.abs(Number(diff.asMonths().toFixed(0)))
+        if (diffRes === 0) {
+          unit = 'week'
+          finalRes = handleDiff(unit)
+          break
+        }
+        if (Math.abs(diffRes) >= 12) {
+          finalRes = cTime
+          break
+        }
+        finalRes = `${Math.abs(diffRes)} ${lang === 'zh-cn' ? '月' : 'months'}`
+        break
+      default:
+        break
+    }
+    return finalRes
+  }
+  res = handleDiff(unit)
+  res = `${res}${isBefore ? lang === 'zh-cn' ? '前' : ' ago' : lang === 'zh-cn' ? '后' : ' after'}`
+  return res
 }
